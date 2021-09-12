fe651f05b73f5b2111a53413c72e07d758c52b10
rtiare <ttorosa55@gmail.com>
2021-09-11 20:38:36 -0700

    created vaccine model

--- VaccineApp/Models/Vaccine.cs
+++ VaccineApp/Models/Vaccine.cs
@@ -7,5 +7,33 @@ namespace VaccineApp.Models
 {
     public class Vaccine
     {
+        //setters and getters
+        public int Id { get; set; }
+        public string VaccineName { get; set; }
+        public int DoseRequired { get; set; }
+        public int? DaysBtwDose { get; set; }
+        public int TotalDose { get; set; } = default;
+        public int DoseLeft { get; set; } = default;
+
+        public Vaccine() { }
+
+        public Vaccine(string name, int dose, int? days, int total, int left)
+        {
+            VaccineName = name;
+            DoseRequired = dose;
+            DaysBtwDose = days;
+            TotalDose = total;
+            DoseLeft = left;
+        }
+
+        public Vaccine(int id, string name, int dose, int? days, int total, int left)
+        {
+            Id = id;
+            VaccineName = name;
+            DoseRequired = dose;
+            DaysBtwDose = days;
+            TotalDose = total;
+            DoseLeft = left;
+        }
     }
 }
