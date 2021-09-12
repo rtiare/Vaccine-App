34eab0101a1558d3616e877a6ef4416b4b5f5c9f
rtiare <ttorosa55@gmail.com>
2021-09-11 20:08:20 -0700

    created controller classes

---
+++ VaccineApp/Controllers/VaccinesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VaccineApp.Controllers
+{
+    public class VaccinesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
