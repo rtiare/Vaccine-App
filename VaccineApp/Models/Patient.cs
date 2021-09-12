56a35cec09dbcb954b76b731912d152c63ec4488
rtiare <ttorosa55@gmail.com>
2021-09-11 20:36:49 -0700

    created patient model

--- VaccineApp/Models/Patient.cs
+++ VaccineApp/Models/Patient.cs
@@ -7,5 +7,22 @@ namespace VaccineApp.Models
 {
     public class Patient
     {
+        //setters and getters
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Vaccineid { get; set; }
+        public string FirstDose { get; set; }
+        public string SecondDose { get; set; }
+
+        public Patient() { }
+
+        //create another class with id
+        public Patient(string name, string vaccine, string firstDose, string secondDose)
+        {
+            Name = name;
+            Vaccineid = vaccine;
+            FirstDose = firstDose;
+            SecondDose = secondDose;
+        }
     }
 }
